774a696bb9385adee90277a8b8ebb1bc5729af4c
irllamartins <irllamartinsbarbosa@gmail.com>
2023-02-27 21:25:18 -0300

    notificação de segundo plano funcionando

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,2 +1,28 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js'); 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDEwnbp_HJUx3Lsox56kB_4QMvioZGiT5o",
+    authDomain: "estudo-notificacao.firebaseapp.com",
+    projectId: "estudo-notificacao",
+    storageBucket: "estudo-notificacao.appspot.com",
+    messagingSenderId: "613507036757",
+    appId: "1:613507036757:web:eb31f187df1ff1725746dc"
+  };
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
